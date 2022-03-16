f55bc780e93a5fb928b9734e34ebadc4f24b0826
aldrintm <aldrin@plamarusa.com>
2022-03-16 10:13:42 -0700

    adding single user page

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -13,21 +13,53 @@ export const GithubProvider = ({ children }) => {
 
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  const fetchUsers = async () => {
+  // Get initial users; Use this for testing purposes on;ly
+  // const fetchUsers = async () => {
+  //   setLoading()
+
+  //   const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  //     },
+  //   })
+
+  //   const data = await response.json()
+
+  //   // Previously used with useState
+  //   // setUsers(data)
+  //   // setLoading(false)
+
+  //   dispatch({
+  //     type: 'GET_USERS',
+  //     payload: data,
+  //   })
+  // }
+
+  // Get Search Results
+  const searchUsers = async (text) => {
     setLoading()
 
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    })
+    const params = new URLSearchParams({ q: text })
 
-    const data = await response.json()
+    const response = await fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    )
+    // if you check postman with the fetch api above we get an JSON object of results from data
+    // we are only interested in the "items" array, so we need to just grab that
+    // so instead of using "data", we can destructure items in data by using {items}
+    // const data = await response.json()
+    const { items } = await response.json()
 
     // Previously used with useState
     // setUsers(data)
@@ -35,15 +67,46 @@ export const GithubProvider = ({ children }) => {
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      // payload: data, "this part too, since we no longer use data as var; we can replace it with items as destructured above"
+      payload: items,
     })
   }
 
-  // Set loading
+  // Get a Single User Page Result
+  const getUser = async (login) => {
+    setLoading()
 
+    const response = await fetch(
+      `${process.env.REACT_APP_GITHUB_URL}/users/${login}`,
+      {
+        headers: {
+          Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      }
+    )
+
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+      const data = await response.json()
+
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      })
+    }
+  }
+
+  // Set loading
   const setLoading = () =>
     dispatch({
       type: 'SET_LOADING',
+    })
+
+  // Clear users from State
+  const clearUsers = () =>
+    dispatch({
+      type: 'CLEAR_USERS',
     })
 
   return (
@@ -51,7 +114,10 @@ export const GithubProvider = ({ children }) => {
       value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        user: state.user,
+        searchUsers,
+        clearUsers,
+        getUser,
       }}
     >
       {children}
